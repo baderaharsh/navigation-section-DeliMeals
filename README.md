c6b3e2ee8cef1ca27e25054589cf07bdfb29f9a3
Harsh Badera <baderaharsh@gmail.com>
2020-07-09 17:29:00 +0530

    For testing navigation, added a new sample page

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ This application is to explore various dishes/meals of different categories. Thi
 ###########################################################################################
 
 Step 1 - Created Home page (categories_page.dart) and added GridView for categories in it.
-Step 2 - Styling and theming GridView. Added new Font styles for Caregory titles. 
+Step 2 - Styling and theming GridView. Added new Font styles for Caregory titles.
+Step 3 - Added another page. On clicking any category, you will be redirected to a sample page. This is implemented using Navigator class and push() method taking MaterialPageRoute() arguement.
+
 
--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_page.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,21 +7,37 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsPage();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.title,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.1), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: color,
+      // splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.1), color.withOpacity(0.8)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
